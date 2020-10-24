928b0283cb1f5b4976f6c281e823c45bc37db4d2
shimada <ue113241@gmail.com>
2020-10-24 05:29:28 +0000

    2

--- js/home.js
+++ js/home.js
@@ -1,3 +1,12 @@
+// home
 $('.nav_toggle').on('click', function () {
     $('.nav_toggle, .nav').toggleClass('show');
 });
+
+$('.nav_menu_li').hover(function () {
+    // カーソルが当たった時の処理
+    $(this).css({"text-decoration":"underline"});
+    }, function() {
+    // カーソルが離れた時の処理
+    $(this).css("text-decoration", "none");
+})
